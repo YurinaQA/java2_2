8049af104aac88470e74721b3865df6d6d59debc
Test <irischj@mail.ru>
2024-02-23 02:12:20 +0500

    src/

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int bilet = 13600;
-        int bonusMiles = 20;
+        int initialData = 50; // Сумма на счету у клиента
+        int bonusScale = 100; // Бонусная шкала - 1 бонус за каждые полные 100 рублей пополения
+        int deposit = 1700; // Сумма пополнения
+        int bonus;
 
-        int miles = bilet / bonusMiles;
-        System.out.println("Количество начисленных миль за купленный билет:" + miles);
+        if (deposit > 1000) {
+            bonus = deposit / bonusScale;
+        }
+        else {
+            bonus = 0;
+        }
+
+        int totalAccount = initialData + deposit + bonus;
+
+        System.out.println("Пополнили счёт на " + deposit + " руб.");
+        System.out.println("Бонус равен " + bonus + " руб.");
+        System.out.println("Итоговая сумма " + totalAccount + " руб.");
     }
 }
