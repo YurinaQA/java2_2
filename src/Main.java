3aacb8a01d890ff9cb7441b2f6174067c838e3b6
Test <irischj@mail.ru>
2024-02-23 01:00:26 +0500

    .gitignore

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int bilet = 13600;
+        int bonusMiles = 20;
+
+        int miles = bilet / bonusMiles;
+        System.out.println("Количество начисленных миль за купленный билет:" + miles);
+    }
+}
